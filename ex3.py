52f329ac4d0b3cda9aebd4df4eac8c356811c472
senakilinc <sena.kilinc.001@student.uni.lu>
2022-05-18 01:34:35 +0200

    New version of ex3.py

--- ex3.py
+++ ex3.py
@@ -1,166 +1,128 @@
-import numpy
+import torch
+import torchvision
+import numpy as np
+import matplotlib.pyplot as plt
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision.datasets import FashionMNIST
+from torchvision.transforms import ToTensor
+from torchvision.utils import make_grid
+from torch.utils.data.dataloader import DataLoader
+from torch.utils.data import random_split
+%matplotlib inline
 
-class TwoHiddenLayerNeuralNetwork:
-    def __init__(self, input_array: numpy.ndarray, output_array: numpy.ndarray) -> None:
+data = FashionMNIST(root='data/', download=True, transform=ToTensor())
 
-        # Input values provided for training the model.
-        self.input_array = input_array
+validation_size = 10000
+train_size = len(data) - validation_size
 
-        # Random initial weights are assigned.
-        self.input_layer_and_first_hidden_layer_weights = numpy.random.rand(
-            self.input_array.shape[1], 4 # First hidden layer consists of 4 nodes
-        )
+train_data, val_data = random_split(data, [train_size, validation_size])
 
-        self.first_hidden_layer_and_second_hidden_layer_weights = numpy.random.rand(
-            4, 3         # First hidden layer has 4 nodes, second hidden layer has 3 nodes.
-        )
+batch_size=128
 
-        self.second_hidden_layer_and_output_layer_weights = numpy.random.rand(3, 1)
-
-        self.output_array = output_array #real output values
-
-        self.predicted_output = numpy.zeros(output_array.shape)
-
-        # Layer_between_input_and_first_hidden_layer is the layer connecting the input nodes with the first hidden layer nodes.
-        self.layer_between_input_and_first_hidden_layer = sigmoid(
-            numpy.dot(self.input_array, self.input_layer_and_first_hidden_layer_weights)
-        )
-
-        # layer_between_first_hidden_layer_and_second_hidden_layer is the layer connecting the first hidden set of nodes with the second hidden set of nodes.
-        self.layer_between_first_hidden_layer_and_second_hidden_layer = sigmoid(
-            numpy.dot(
-                self.layer_between_input_and_first_hidden_layer,
-                self.first_hidden_layer_and_second_hidden_layer_weights,
-            )
-        )
-
-        # layer_between_second_hidden_layer_and_output is the layer connecting second hidden layer with the output node.
-        self.layer_between_second_hidden_layer_and_output = sigmoid(
-            numpy.dot(
-                self.layer_between_first_hidden_layer_and_second_hidden_layer,
-                self.second_hidden_layer_and_output_layer_weights,
-            )
-        )
-
-        return self.layer_between_second_hidden_layer_and_output
-
-    def back_propagation(self) -> None:
-
-        updated_second_hidden_layer_and_output_layer_weights = numpy.dot(
-            self.layer_between_first_hidden_layer_and_second_hidden_layer.T,
-            2
-            * (self.output_array - self.predicted_output)
-            * sigmoid_derivative(self.predicted_output),
-        )
-        updated_first_hidden_layer_and_second_hidden_layer_weights = numpy.dot(
-            self.layer_between_input_and_first_hidden_layer.T,
-            numpy.dot(
-                2
-                * (self.output_array - self.predicted_output)
-                * sigmoid_derivative(self.predicted_output),
-                self.second_hidden_layer_and_output_layer_weights.T,
-            )
-            * sigmoid_derivative(
-                self.layer_between_first_hidden_layer_and_second_hidden_layer
-            ),
-        )
-        updated_input_layer_and_first_hidden_layer_weights = numpy.dot(
-            self.input_array.T,
-            numpy.dot(
-                numpy.dot(
-                    2
-                    * (self.output_array - self.predicted_output)
-                    * sigmoid_derivative(self.predicted_output),
-                    self.second_hidden_layer_and_output_layer_weights.T,
-                )
-                * sigmoid_derivative(
-                    self.layer_between_first_hidden_layer_and_second_hidden_layer
-                ),
-                self.first_hidden_layer_and_second_hidden_layer_weights.T,
-            )
-            * sigmoid_derivative(self.layer_between_input_and_first_hidden_layer),
-        )
-
-        self.input_layer_and_first_hidden_layer_weights += (
-            updated_input_layer_and_first_hidden_layer_weights
-        )
-        self.first_hidden_layer_and_second_hidden_layer_weights += (
-            updated_first_hidden_layer_and_second_hidden_layer_weights
-        )
-        self.second_hidden_layer_and_output_layer_weights += (
-            updated_second_hidden_layer_and_output_layer_weights
-        )
-
-    def train(self, output: numpy.ndarray, iterations: int, give_loss: bool) -> None:
-
-        for iteration in range(1, iterations + 1):
-            self.output = self.feedforward()
-            self.back_propagation()
-            if give_loss:
-                loss = numpy.mean(numpy.square(output - self.feedforward()))
-                print(f"Iteration {iteration} Loss: {loss}")
-
-    def predict(self, input: numpy.ndarray) -> int:
-
-        self.array = input # Input values for which the predictions are to be made.
-
-        self.layer_between_input_and_first_hidden_layer = sigmoid(
-            numpy.dot(self.array, self.input_layer_and_first_hidden_layer_weights)
-        )
-
-        self.layer_between_first_hidden_layer_and_second_hidden_layer = sigmoid(
-            numpy.dot(
-                self.layer_between_input_and_first_hidden_layer,
-                self.first_hidden_layer_and_second_hidden_layer_weights,
-            )
-        )
-
-        self.layer_between_second_hidden_layer_and_output = sigmoid(
-            numpy.dot(
-                self.layer_between_first_hidden_layer_and_second_hidden_layer,
-                self.second_hidden_layer_and_output_layer_weights,
-            )
-        )
-        print("Model value:", self.layer_between_second_hidden_layer_and_output)
-        return int(self.layer_between_second_hidden_layer_and_output > 0.6)
+train_loader = DataLoader(train_data, batch_size, shuffle=True, num_workers=4, pin_memory=True)
+val_loader = DataLoader(val_data, batch_size*2, num_workers=4, pin_memory=True)
 
 
-def sigmoid(value: numpy.ndarray) -> numpy.ndarray:
+class FFNN(nn.Module):
+    def __init__(self, input_size, num_hidden_layers, hidden_size, out_size, accuracy_function):
+        super().__init__()
+        self.accuracy_function = accuracy_function
+        self.input_layer = nn.Linear(input_size, hidden_size)
 
-    return 1 / (1 + numpy.exp(-value)) #appliying sigmoid activation function
+        self.hidden_layers = nn.ModuleList()
+        for i in range(0, num_hidden_layers):
+            self.hidden_layers.append(nn.Linear(hidden_size, hidden_size))
+
+        self.output_layer = nn.Linear(hidden_size, out_size)
+
+    def forward(self, input_image):  # connects all layers we defined in the constructor
+        input_image = input_image.view(input_image.size(0), -1)
+
+        output = self.input_layer(input_image)
+        output = F.relu(output)  # ReLu activation function
+
+        for layer in self.hidden_layers:
+            output = layer(output)
+            output = F.relu(output)
+
+        output = self.output_layer(output)
+        return output
+
+    def training_step(self,
+                      batch):  # this function takes the batch of images by the DataLoader and pushes them to get the prediction
+        images, labels = batch
+        output = self(images)
+        loss = F.cross_entropy(output, labels)  # cross_entropy omputes the cross entropy loss between input and target
+        return loss
+
+    def validation_step(self, batch):
+        images, labels = batch
+        output = self(images)
+        loss = F.cross_entropy(output, labels)
+        acc = self.accuracy_function(output, labels)
+
+        return {'val_loss': loss, 'val_acc': acc}
+
+    def validation_epoch_end(self, outputs):
+        batch_losses = [x['val_loss'] for x in outputs]
+        epoch_loss = torch.stack(batch_losses).mean()  # combine losses and return mean value
+
+        batch_accs = [x['val_acc'] for x in outputs]  # Combine accuracies and return mean value
+        epoch_acc = torch.stack(batch_accs).mean()
+        return {'val_loss': epoch_loss.item(), 'val_acc': epoch_acc.item()}
+
+    def epoch_end(self, epoch, result):
+        print("Epoch: {} - Validation Loss: {:.4f}, Validation Accuracy: {:.4f}".format(epoch, result['val_loss'],
+                                                                                        result['val_acc']))
 
 
-def sigmoid_derivative(value: numpy.ndarray) -> numpy.ndarray:
+class ModelTrainer():
+    def fit(self, epochs, learning_rate, model, train_loader, val_loader, opt_func=torch.optim.SGD):
+        history = []
+        optimizer = opt_func(model.parameters(), learning_rate)
 
-    return (value) * (1 - (value))
+        for epoch in range(epochs):
+            for batch in train_loader:
+                loss = model.training_step(batch)
+                loss.backward()
+                optimizer.step()
+                optimizer.zero_grad()
+            result = self._evaluate(model, val_loader)
+            model.epoch_end(epoch, result)
+            history.append(result)
 
+        return history
 
-def example() -> int:
+    def _evaluate(self, model, val_loader):
+        outputs = [model.validation_step(batch) for batch in val_loader]
+        return model.validation_epoch_end(outputs)
 
-    # Input values.
-    input = numpy.array(
-        (
-            [0, 0, 0],
-            [0, 0, 1],
-            [0, 1, 0],
-            [0, 1, 1],
-            [1, 0, 0],
-            [1, 0, 1],
-            [1, 1, 0],
-            [1, 1, 1],
-        ),
-        dtype=numpy.float64,
-    )
+def accuracy(outputs, labels):
+    _, preds = torch.max(outputs, dim=1)
+    return torch.tensor(torch.sum(preds == labels).item() / len(preds))
 
-    #True output values for the given input values.
-    output = numpy.array(([0], [1], [1], [0], [1], [0], [0], [1]), dtype=numpy.float64)
+def plot_history(history):
+    losses = [x['val_loss'] for x in history]
+    plt.plot(losses, '-x')
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
 
-    neural_network = TwoHiddenLayerNeuralNetwork(input_array=input, output_array=output)
+    accuracies = [x['val_acc'] for x in history]
+    plt.plot(accuracies, '-x')
+    plt.xlabel('epoch')
+    plt.ylabel('accuracy')
+    plt.title('Loss and Accuracy');
 
-    neural_network.train(output=output, iterations=10, give_loss=True) #give_loss is set to true to see the loss at every iteration
+input_size = 784
+num_classes = 10
 
-    return neural_network.predict(numpy.array(([0, 0, 0]), dtype=numpy.float64))
+model = FFNN(input_size, 3, 32, out_size=num_classes, accuracy_function=accuracy)
+print(model)
 
+model_trainer = ModelTrainer()
 
-if __name__ == "__main__":
-    print(example())
+training_history = []
+training_history += model_trainer.fit(10, 0.2, model, train_loader, val_loader)
+
+plot_history(training_history)
